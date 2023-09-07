e36977fbc36735f4d213fa29c1ffd49659145533
srihitha-ravi <s.ravi@pvp.co.jp>
2023-09-07 09:12:20 +0900

    added create issue button func

--- lib/Create Issue/create_issue.dart
+++ lib/Create Issue/create_issue.dart
@@ -1,23 +1,33 @@
+import 'package:bughub_demo/Theme/appbar_theme.dart';
 import 'package:flutter/material.dart';
 
 class CreateIssuePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Issue'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Add your content for creating an issue here
-            Text(
-              'Create Issue Page',
-              style: TextStyle(fontSize: 24),
+    return Theme(
+      data: appThemeData,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Create Issue',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
             ),
-            // Add form fields, buttons, or any other UI elements needed for issue creation
-          ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Add your content for creating an issue here
+              Text(
+                'Create Issue Page',
+                style: TextStyle(fontSize: 24),
+              ),
+              // Add form fields, buttons, or any other UI elements needed for issue creation
+            ],
+          ),
         ),
       ),
     );
